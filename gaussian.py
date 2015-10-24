8a086fb8b53f9fc221e49cd96cb9b04e2e87bf20
Pranav Shyam <pranavshyam13@gmail.com>
2015-10-24 22:02:21 +0530

    utils update

--- gaussian.py
+++ gaussian.py
@@ -1,29 +1,57 @@
-import numpy as np
-
+import autograd.numpy as np
+from autograd import grad
 import matplotlib.pyplot as plt
 plt.ion()
 
-def activate(locations, address):
-	# Gaussian
-	sigma = 0.01
+def gaussian(locations, address):
+	sigma = 0.05
 	mu = address
 	norm = (locations - mu) ** 2
-	fac = -np.sum(norm / (2 * (sigma ** 2)), axis=1, keepdims=True)
+	fac = -np.sum(norm / (2 * (sigma ** 2)))
 	return np.exp(fac)
 
 
+def deccan(locations, mu):
+	sigma = 0.05
+	mu = address
+	norm = (locations - mu) ** 2
+	fac = -np.sum(norm / (2 * (sigma ** 2)))
+	return (np.exp(fac) + sigma / (norm + 0.999))
+
+
+gaussian_grad = grad(gaussian)
+deccan_grad = grad(deccan)
+
 address = 0.0
-locations = np.linspace(-0.2, 0.2, 10000).reshape(-1, 1)
-triggers = activate(locations, address)
-plt.plot(locations, triggers)
+locations = np.linspace(-2, 2, 1000).reshape(-1, 1)
 
-print 'max: ', activate(np.array([[address]]), address)
+triggers = []
+for i in range(1000):
+	G = gaussian(locations[i], address)
+	triggers.append(G)
 
-address = 0.1
-locations = np.linspace(-0.2, 0.2, 10000).reshape(-1, 1)
-triggers = activate(locations, address)
-plt.plot(locations, triggers)
+plt.plot(locations, triggers, label='Gaussian')
 
-print 'max: ', activate(np.array([[address]]), address)
+print 'Gaussian:'
+print '\tmax: ', gaussian(np.array([[0.0]]), address)
+print '\tvalue at .50: ', gaussian(np.array([[-5.0]]), address)
+print '\tnear: ', gaussian_grad(np.array([[-1.0]]), address)
+print '\tfar: ', gaussian_grad(np.array([[-5.0]]), address)
+
+
+triggers = []
+for i in range(1000):
+	G = deccan(locations[i], address)
+	triggers.append(G)
+
+plt.plot(locations, triggers, label='Deccan')
+
+print 'Deccan: '
+print '\tmax: ', deccan(np.array([[0.0]]), address)
+print '\tvalue at .50: ', deccan(np.array([[-0.50]]), address)
+print '\tnear: ', deccan_grad(np.array([[-1.0]]), address)
+print '\tfar: ', deccan_grad(np.array([[-0.50]]), address)
+
+plt.legend()
 
 raw_input()
